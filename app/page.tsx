0e39997294063451da037ea63151932238cfe9c1
JustBecerra <justj.becerra@gmail.com>
2024-04-20 16:07:34 -0300

    add character selected

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,29 @@ import RandMContext from "@/context/RandMContext";
 import { useContext } from "react";
 
 export default function Home() {
-  const { characters, episodes } = useContext(RandMContext);
+  const {
+    characters,
+    episodes,
+    setCharacter1,
+    setCharacter2,
+    character1,
+    character2,
+  } = useContext(RandMContext);
   console.log({ episodes });
   return (
     <main className="flex min-h-screen flex-col items-center gap-4">
       <Header />
       <div className="flex w-[80%] h-[50%] gap-4">
-        <CharacterList characters={characters} />
-        <CharacterList characters={characters} />
+        <CharacterList
+          characters={characters}
+          chosenCharacter={character1}
+          setCharacter={setCharacter1}
+        />
+        <CharacterList
+          characters={characters}
+          chosenCharacter={character2}
+          setCharacter={setCharacter2}
+        />
       </div>
       <div className="flex w-[100%] h-[50%] justify-center gap-4">
         <EpisodeCard episodes={episodes} />
--- components/CharacterCard/index.tsx
+++ components/CharacterCard/index.tsx
@@ -1,24 +1,57 @@
+import { CharactersType } from "@/types";
 import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
 
 type CardProps = {
-  name: string;
-  image: string;
-  species: string;
-  status: string;
+  char: CharactersType;
+  setCharacter: Dispatch<SetStateAction<CharactersType | null>>;
+  chosenCharacter: CharactersType | null;
 };
 
 export const CharacterCard = (props: CardProps) => {
-  const { name, image, species, status } = props;
+  const { name, image, species, status, id } = props.char;
+  const { setCharacter, chosenCharacter } = props;
+
+  const characterSelection = () => {
+    if (chosenCharacter && chosenCharacter.id === id) {
+      setCharacter(null);
+    } else {
+      setCharacter(props.char);
+    }
+  };
   return (
-    <div className="flex w-[45%] h-[150px] border-2 border-gray-50 m-2">
+    <div
+      className={`flex w-[45%] h-[150px] border-2 border-gray-50 m-2 cursor-pointer ${
+        chosenCharacter && chosenCharacter.id === id && "bg-gray-50"
+      }`}
+      onClick={characterSelection}
+    >
       <div className="w-[50%] h-[100%] relative">
         <Image src={image} alt="" fill />
       </div>
       <div className="flex flex-col items-center justify-center w-[50%]">
-        <h1 className="text-sm text-balance text-center">{name}</h1>
+        <h1
+          className={`text-sm text-balance text-center ${
+            chosenCharacter && chosenCharacter.id === id && "text-black"
+          }`}
+        >
+          {name}
+        </h1>
         <div className="flex gap-2">
-          <h3 className="text-xs">{species}</h3>
-          <h3 className="text-xs">{status}</h3>
+          <h3
+            className={`text-xs ${
+              chosenCharacter && chosenCharacter.id === id && "text-black"
+            }`}
+          >
+            {species}
+          </h3>
+          <h3
+            className={`text-xs ${
+              chosenCharacter && chosenCharacter.id === id && "text-black"
+            }`}
+          >
+            {status}
+          </h3>
         </div>
       </div>
     </div>
