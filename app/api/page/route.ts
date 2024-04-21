32f74bd2828108ce189b0953817eb26b706b5516
JustBecerra <justj.becerra@gmail.com>
2024-04-21 12:15:36 -0300

    add route handler

--- app/api/page/route.ts
+++ app/api/page/route.ts
@@ -1,3 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 const GET = async (page: number) => {
   const res = await fetch(
     `https://rickandmortyapi.com/api/character/?page=${page}`
@@ -9,4 +11,22 @@ const GET = async (page: number) => {
   return Response.json(data);
 };
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { page } = req.query; // Assuming you're passing the page number as a query parameter
+  if (typeof page !== "string") {
+    res.status(400).json({ error: "Invalid page number" });
+    return;
+  }
+  try {
+    const pageNumber = parseInt(page, 10);
+    const data = await GET(pageNumber);
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+}
 export { GET };
